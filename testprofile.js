28b0cb032fbc7648042c02a374421fcc06c70ad0
Aditya_Mahendra_Kalbhor <aditya.mahendra-kalbhor@capgemini.com>
2026-08-03 21:32:49 +0530

    Datatype's

---
+++ testprofile.js
@@ -0,0 +1,13 @@
+let name1 ;
+name1 =" Aditya";
+const id1 = 1001;
+let date = "12-12-2020"
+let name2 ="Nitin"
+const id2 =1002;
+console.log("=======================================================")
+console.log("            __CLASS__DATA__ ")
+console.log("=======================================================")
+console.log(" First name Id and date is as follows :-",name1 +", "+ id1 +", "+ date);
+console.log("=======================================================")
+console.log("Second name and id is as follows:- ", name2 +", "+ id2);
+console.log("=======================================================")
