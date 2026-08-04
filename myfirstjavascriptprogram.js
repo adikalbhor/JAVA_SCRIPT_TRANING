62e2d7bfc4a900814cbf71240b570850980062cd
Aditya_Mahendra_Kalbhor <aditya.mahendra-kalbhor@capgemini.com>
2026-08-04 19:27:42 +0530

    new

--- myfirstjavascriptprogram.js
+++ myfirstjavascriptprogram.js
@@ -2,7 +2,7 @@ console.log(" Hello Java Script ");
 let email ="Adit@gmail.com"; // data type here i am declearing and initializaing 
 console.log(email); 
 let salary = 65000000109800000000000000123000000003450000000000432;
-let salary =3.4; // cannot redeclear the same variable again
+//let salary =3.4; // cannot redeclear the same variable again
 console.log("My Salary will be " + salary);
 let date="14-02-26";
 console.log("So the date will be" +date);
